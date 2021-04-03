f399abd34c35f8f3be7d690d4d54a62a277a1f9d
mateugazi <mateugazi182@outlook.com>
2021-04-03 19:25:33 +0200

    Created groupManagement template

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import {Provider} from 'react-redux';
 import store from './app/store';
+import { GroupManagement } from './components/groupManagement/groupManagement';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <GroupManagement />
       </div>
     </Provider>
   );
---
+++ src/components/groupManagement/groupManagement.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const GroupManagement = () => {
+    
+    return (
+        <div>
+            yo
+        </div>
+    )
+}
