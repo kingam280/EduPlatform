f8dde1627d6039b13129d9d55e2624ac700c1e1f
MaDud <martaduda@interia.eu>
2021-04-13 21:24:01 +0200

    Add router guards

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,34 @@ import ProjectCard from './components/ProjectsPage/ProjectCard';
 import ProjectsPage from './components/ProjectsPage/ProjectsPage';
 import Tasks from './components/Tasks/tasks';
 import Menu from './components/Navigation/menu';
-import ChangeForm from "./components/Authorization/ChangeForm"
+import ChangeForm from "./components/Authorization/ChangeForm";
+import { useSelector } from 'react-redux';
+import { RootState } from './app/rootReducer';
 
 function App() {
+
+  const token = useSelector((state:RootState) => state.authorization.token);
+
   return (
     <Provider store={store}>
       <div className="App">
         <Router>
           <Menu />
-          <Switch>
-            <Route exact path="/projects">
-              <ProjectsPage />
-            </Route>
-            <Route path="/projects/:projectId">
-              <ProjectCard />
-              <Tasks />
-            </Route>
-            <Route exact path="/">
-              <ChangeForm />
-            </Route> 
-          </Switch>
+          {token ? (
+            <Switch>
+              <Route exact path="/projects">
+                <ProjectsPage />
+              </Route>
+              <Route path="/projects/:projectId">
+                <ProjectCard />
+                <Tasks />
+              </Route>
+            </Switch>
+          ) 
+          : ( <Route exact path="/">
+                <ChangeForm />
+              </Route> 
+          )}
         </Router>
       </div>
     </Provider>
