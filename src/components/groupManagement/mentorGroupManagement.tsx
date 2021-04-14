67994f6fd09bcf2c0b02a357785db8f5a7958993
mateugazi <mateugazi182@outlook.com>
2021-04-15 01:42:01 +0200

    Created groupManagement section for mentor

---
+++ src/components/groupManagement/mentorGroupManagement.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { GroupCreate } from './groupCreate';
+import useGetMentors from '../../hooks/useGetMentors';
+import { User } from '../../types/user';
+import useGetData from '../../hooks/useGetData';
+import { Groups } from '../../types/groups';
+
+export const MentorGroupManagement = () => {
+    const groups = useGetData<Groups>('group');
+    const mentors = useGetMentors('authorization');
+    const users = useGetData<User[]>('authorization')
+    return (
+        <>
+        {console.log(groups, mentors)}
+               {groups ?  mentors && users ?  <GroupCreate mentors={mentors} groups={groups} users={users}/>: "loading" : "loading"}
+           
+        </>
+    );
+}
